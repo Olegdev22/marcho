9e5bf9361c4db463f88914853899d4bbda9c4f14
Olegdev22 <kirbaba2006@ukr.net>
2022-12-15 15:48:12 +0200

    add categories section

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,9 +1,34 @@
 $(function() {
-    $('.top-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 50) {
+      $('.header__inner').addClass('sticky')
+    } else {
+      $('.header__inner').removeClass('sticky')
+    }
+  });
+
+  let headerHeight = $('.header__inner').outerHeight();
+
+  $('.menu__link').on('click', function(event) {
+
+    event.preventDefault();
+
+    let id = $(this).attr('href'),
+
+      top = $(id).offset().top - headerHeight;
+
+    $('body,html').animate({
+      scrollTop: top
+    }, 1000);
+  });
+
+
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 });
